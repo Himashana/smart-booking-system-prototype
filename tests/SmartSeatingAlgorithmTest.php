bc2fb35ab6dba3c4fdae784a548afa7984390e6b
Himashana <himashanasuraweera5@gmail.com>
2025-05-31 19:29:34 +0530

    feat: new unit tests

--- tests/SmartSeatingAlgorithmTest.php
+++ tests/SmartSeatingAlgorithmTest.php
@@ -155,5 +155,160 @@
             $actual = SmartSeatingAlgorithm::predictSeats($matrix, audienceTypes['GROUP'], theatreSections['MEZZANINE'], 10);
             $this->assertEquals($expected, $actual);
         }
+
+        public function test_couple_single_row_separated_seats()
+        {
+            $matrix = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 0, 1, 0, 1, 1],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            
+            $expected = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 0, 1, 0, 1, 1],
+                [1, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            $actual = SmartSeatingAlgorithm::predictSeats($matrix, audienceTypes['COUPLE'], theatreSections['MEZZANINE'], 2);
+            $this->assertEquals($expected, $actual);
+        }
+
+        public function test_couple_single_row_multiple_empty_seats()
+        {
+            $matrix = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            
+            $expected = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            $actual = SmartSeatingAlgorithm::predictSeats($matrix, audienceTypes['COUPLE'], theatreSections['MEZZANINE'], 2);
+            $this->assertEquals($expected, $actual);
+        }
+
+        // Assuming VIP seats are always in either sides of the last row of the balcony section:
+        // [VIP, VIP, -, -, -, VIP, VIP]
+        public function test_vip_seats_booking()
+        {
+            $matrix = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            
+            $expected = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0, 0, 0]
+            ];
+            $actual = SmartSeatingAlgorithm::predictSeats($matrix, audienceTypes['VIP'], "", 1);
+            $this->assertEquals($expected, $actual);
+        }
+
+        public function test_disabled_person_seats_booking()
+        {
+            $matrix = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            
+            $expected = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            $actual = SmartSeatingAlgorithm::predictSeats($matrix, audienceTypes['DISABLED_PERSON'], "", 1);
+            $this->assertEquals($expected, $actual);
+        }
+
+        public function test_patron_seats_booking()
+        {
+            $matrix = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            
+            $expected = [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0]
+            ];
+            $actual = SmartSeatingAlgorithm::predictSeats($matrix, audienceTypes['PATRON'], "", 1);
+            $this->assertEquals($expected, $actual);
+        }
+
+        // Seats for special peoples will be allocated for normal audiance if other seats are not available.
+        public function test_single_all_booked_empty_special_seats()
+        {
+            $matrix = [
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 0, 0], // <- Special seats for disabled persons
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+            ];
+            
+            $expected = [
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 0], // <- Special seats for disabled persons
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+            ];
+            $actual = SmartSeatingAlgorithm::predictSeats($matrix, audienceTypes['SINGLE'], "", 1);
+            $this->assertEquals($expected, $actual);
+        }
+
+        // No tests for admins as they have the option to book any seat without restrictions.
     }
 ?>
