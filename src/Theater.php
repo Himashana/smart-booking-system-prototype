2efc1e931eb601cb51f33d49b35f0c80ba172bb5
Himashana <himashanasuraweera5@gmail.com>
2025-06-02 00:04:26 +0530

    feat: new methods in the theater class to add and get theaters

--- src/Theater.php
+++ src/Theater.php
@@ -2,11 +2,53 @@
     include($filePathPrefix . 'dbConfig.php');
 
     class Theater {
-        function addTheater() {
+        function addTheater($name, $rows, $columns) {
             $dbconnection = new DBconnect();
             $dbconnection->MakeConn();
-            echo "Connected successfully";
+            
+            $query = 'INSERT INTO theaters (name, t_rows, t_cols) VALUES ("' . $name . '", ' . $rows . ', ' . $columns . ')';
+            $result = $dbconnection->ExecuteQuery($query);
+
             $dbconnection->CloseConn();
+
+            if ($result) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function getTheaterById($id) {
+            $dbconnection = new DBconnect();
+            $dbconnection->MakeConn();
+            
+            $query = 'SELECT * FROM theaters WHERE id = ' . $id;
+            $result = $dbconnection->ExecuteQuery($query);
+
+            if ($row = mysqli_fetch_assoc($result)) {
+                $dbconnection->CloseConn();
+                return $row;
+            } else {
+                $dbconnection->CloseConn();
+                return null;
+            }
+        }
+
+        function getTheaters() {
+            $dbconnection = new DBconnect();
+            $dbconnection->MakeConn();
+            
+            $query = 'SELECT * FROM theaters';
+            $result = $dbconnection->ExecuteQuery($query);
+
+            $theaters = [];
+            while ($row = mysqli_fetch_assoc($result)) {
+                $theaters[] = $row;
+            }
+
+            $dbconnection->CloseConn();
+
+            return $theaters;
         }
     }
 ?>
